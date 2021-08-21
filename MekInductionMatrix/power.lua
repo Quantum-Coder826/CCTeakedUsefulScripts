60220ceb0eabe6f16fdf5c7867e327545facb878
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-21 11:47:42 +0200

    Update power.lua

--- MekInductionMatrix/power.lua
+++ MekInductionMatrix/power.lua
@@ -22,23 +22,27 @@ if n >= 10^12 then
 end
 
 while true do
+    -- Add text on top
+    modem.callRemote(monitor,"clear");modem.callRemote(monitor,"setCursorPos",1,1)
+    modem.callRemote(monitor,"write","InductionMatrix:")
+
     -- current and maximum power in cell
     CurrentPower = convert(modem.callRemote(cell,"getEnergy"))
     MaxPower = convert(modem.callRemote(cell,"getMaxEnergy"))
-    modem.callRemote(monitor,"clear");modem.callRemote(monitor,"setCursorPos",1,1)
+    modem.callRemote(monitor,"setCursorPos",1,2)
     modem.callRemote(monitor,"write",(CurrentPower.."/"..MaxPower))
     
     -- percentage filled
-    modem.callRemote(monitor,"setCursorPos",modem.callRemote(monitor,"getCursorPos")+1,1)
+    modem.callRemote(monitor,"setCursorPos",modem.callRemote(monitor,"getCursorPos")+1,2)
     Percantage = math.floor(modem.callRemote(cell,"getEnergyFilledPercentage") * 100)
     modem.callRemote(monitor,"write",(Percantage.."%"))
 
     -- RF per second input and output
     Input = convert(modem.callRemote(cell,"getLastInput"))
     Output = convert(modem.callRemote(cell,"getLastOutput"))
-    modem.callRemote(monitor,"setCursorPos",1,2)
-    modem.callRemote(monitor,"write",("In:"..Input.."/t"))
     modem.callRemote(monitor,"setCursorPos",1,3)
+    modem.callRemote(monitor,"write",("In:"..Input.."/t"))
+    modem.callRemote(monitor,"setCursorPos",1,4)
     modem.callRemote(monitor,"write",("Out:"..Output.."/t"))
     sleep(0,8) -- Wait a bit to not overload periferals
 end
