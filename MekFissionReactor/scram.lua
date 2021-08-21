fed629d5c201cb752d944c08ea1de8c547ddc795
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-21 15:49:23 +0200

    Update scram.lua

--- MekFissionReactor/scram.lua
+++ MekFissionReactor/scram.lua
@@ -4,7 +4,8 @@ scram = false
 
 while true do
     if reactor.getDamagePercent() * 100 >= 1 then 
-    reactor.scram(); redstone.setAnalogOutput("left", 1); reactor.setBurnRate(0.1);scram = true
+    reactor.scram(); redstone.setAnalogOutput("left", 1); reactor.setBurnRate(0.1)
+    scram = true
     end
     
     term.clear();term.setCursorPos(1,1)
