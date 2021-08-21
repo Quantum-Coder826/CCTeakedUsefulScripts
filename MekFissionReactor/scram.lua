a1fd93901d4610168fe5b278bb09e9486a7ab12c
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-21 14:57:12 +0200

    Update scram.lua

--- MekFissionReactor/scram.lua
+++ MekFissionReactor/scram.lua
@@ -2,7 +2,8 @@
 reactor = peripheral.wrap("back")
 
 while true do
-    if reactor.getDamagePercent() * 100 => 1 then 
+    if reactor.getDamagePercent() * 100 >= 1 then 
     reactor.scram(); redstone.setAnalogOutput("left", 1)
     end
+    sleep(0,8)
 end
