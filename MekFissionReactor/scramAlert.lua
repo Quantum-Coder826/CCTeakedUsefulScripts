e20842db1e4ec6ef15b033a7124d1c5183401b74
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-21 14:07:08 +0200

    Create scramAlert.lua

---
+++ MekFissionReactor/scramAlert.lua
@@ -0,0 +1,12 @@
+-- periferals config
+modem = peripheral.wrap("back")
+monitor = "monitor_3"
+
+-- start code
+modem.callRemote(monitor,"clear");modem.callRemote(monitor,"setCursorPos",1,1)
+print("Mekanism FissionReactor ScramIndicator \n \nBy QByte")
+
+while true do
+  os.pullEvent("redstone")
+  print("A redstone input has changed!")
+end
