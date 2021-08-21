1ca9c2d338a0603d4670a4ea5962c15a83c0dff7
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-21 16:01:36 +0200

    Update scram.lua

--- MekFissionReactor/scram.lua
+++ MekFissionReactor/scram.lua
@@ -1,23 +1,10 @@
 --Peripherals/variables
 reactor = peripheral.wrap("back")
-scram = false
 
 while true do
     if reactor.getDamagePercent() * 100 >= 1 then 
     reactor.scram();redstone.setAnalogOutput("left", 1)
     reactor.setBurnRate(0.1)
-    scram = true
     end
-    
-    term.clear();term.setCursorPos(1,1)
-    term.write("Reactor Scram System")
-    term.setCursorPos(1,3);term.write("Status:")
-if scram == true then
-    term.setCursorPos(1,(term.getCursorPos() + 1))
-    term.setTextColor(16384);term.write("SCRAM!!")
-else
-    term.setCursorPos(1,(term.getCursorPos() + 1))
-    term.setTextColor(8192);term.write("Online") 
-end
     sleep(0,8)
 end
