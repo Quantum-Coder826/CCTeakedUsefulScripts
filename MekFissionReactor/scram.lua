f6d4eb5a3711c3d27ffe9a102f632e6b2989ed0d
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-08-23 08:47:59 +0200

    Update scram.lua
    Added Reactor WasteTank filling test

--- MekFissionReactor/scram.lua
+++ MekFissionReactor/scram.lua
@@ -4,10 +4,15 @@ reactor = peripheral.wrap("back")
 print("Reactor Scram System")
 
 while true do
-    if reactor.getDamagePercent() * 100 >= 1 then 
+    if reactor.getDamagePercent() * 100 >= 1 then  -- scram reactor when getting damage
     reactor.scram();redstone.setAnalogOutput("left", 1)
     reactor.setBurnRate(0.1)
     print("\nSCRAM!!")
+    end
+
+    if reactor.getWaste() >= 50 then -- scram reactor when wastetank start filling
+    reactor.scram();redstone.setAnalogOutput("left", 1)
+    reactor.setBurnRate(0.1)
     end
     sleep(0,8)
 end
