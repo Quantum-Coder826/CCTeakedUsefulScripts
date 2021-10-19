9bda27033d0cbeaf06e2b7af8095349f25369645
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-10-19 12:37:15 +0200

    Update power.lua

--- MekInductionMatrix/power.lua
+++ MekInductionMatrix/power.lua
@@ -2,12 +2,12 @@
 cell = "inductionMatrix_0"
 monitor = "monitor_0"
 modem = peripheral.wrap("back")
-
+ 
 protocol = "null"
-
+ 
 -- start code
 print("Mekanism InductionMatrix PowerTracker \n \nBy QByte")
-
+ 
 function convert(n) -- Corrects for RF Counter
 if n >= 10^15 then
         return string.format("%.2f PFE", n / 10^15)
@@ -23,7 +23,7 @@ if n >= 10^15 then
         return tostring(n)
     end
 end
-
+ 
 while true do
     -- Get all data
     local CurrentPower = modem.callRemote(cell,"getEnergy")
@@ -31,9 +31,9 @@ while true do
     local Input = modem.callRemote(cell,"getLastInput")
     local Output = modem.callRemote(cell,"getLastOutput")
     local Percantage = math.floor(modem.callRemote(cell,"getEnergyFilledPercentage") * 100)
-
-    remote = CurrentPower .. MaxPower .. Input .. Output .. Percantage
-
+ 
+    remote = CurrentPower .. " " .. MaxPower .. " " .. Input .. " " .. Output .. " " .. Percantage
+ 
     -- format data
     local CurrentPower = convert(CurrentPower)
     local MaxPower = convert(MaxPower)
@@ -43,19 +43,26 @@ while true do
     -- setup monitor
     modem.callRemote(monitor,"clear");modem.callRemote(monitor,"setCursorPos",1,1)
     modem.callRemote(monitor,"write","InductionMatrix:")
-
+ 
     -- write curret power and maximum power
     modem.callRemote(monitor,"setCursorPos",1,2)
     modem.callRemote(monitor,"write",(CurrentPower.."/"..MaxPower))
-
+ 
     -- write percantage filled
     modem.callRemote(monitor,"setCursorPos",modem.callRemote(monitor,"getCursorPos")+1,2)
     modem.callRemote(monitor,"write",(Percantage.."%"))
-
+ 
     -- write current power input and output
     modem.callRemote(monitor,"setCursorPos",1,3)
     modem.callRemote(monitor,"write",("In:"..Input.."/t"))
     modem.callRemote(monitor,"setCursorPos",1,4)
     modem.callRemote(monitor,"write",("Out:"..Output.."/t"))
+    
+    
+    -- Only Debug
+    modem.callRemote(monitor,"setCursorPos",1,5)
+    modem.callRemote(monitor,"write",(remote))
+
+
     sleep(0,8) -- Wait a bit to not overload periferals
 end
