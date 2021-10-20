6ee4bddf07eb5626ab374a5ed05cc802665542f5
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-10-20 10:52:34 +0200

    update format

--- MekInductionMatrix/mobilemonitor.lua
+++ MekInductionMatrix/mobilemonitor.lua
@@ -26,18 +26,14 @@ while true do
     local event, sender, message = os.pullEvent("rednet_message")
     if sender == host then
         for word in string.gmatch(message..tostring(message), '([^,]+)') do
-            if i = 0 then
-                CurrentPower = convert(word)
-            elseif i = 1 then
-                MaxPower = convert(word)
-            elseif i = 2 then
-                Input = convert(word)
-            elseif i = 3 then
-                Output = convert(word)
-            elseif i = 4 then
-                Percantage = convert(word)
+            if i = 0 then CurrentPower = convert(word)
+            elseif i = 1 then MaxPower = convert(word)
+            elseif i = 2 then Input = convert(word)
+            elseif i = 3 then Output = convert(word)
+            elseif i = 4 then Percantage = convert(word)
             end
-            i = i + 1
         end
     end
+
+    
 end
