d932464d9cee87e07f9b54db6e110c694c836386
Quantum-Coder826 <b.veldthuis@gmail.com>
2021-10-20 09:49:24 +0200

    rednet stuff

--- MekInductionMatrix/mobilemonitor.lua
+++ MekInductionMatrix/mobilemonitor.lua
@@ -3,10 +3,11 @@ chunks = {}
 
 -- start code
 rednet.open("back")
+host = rednet.lookup("matrix", "Host")
 
 while true do
-    local event, message, protocol = os.pullEvent("rednet_message")
-    if protocol == "marix" then
+    local event, message, sender = os.pullEvent("rednet_message")
+    if sender == host then
         
         for word in string.gmatch(message, '([^,]+)') do
             print(word)
