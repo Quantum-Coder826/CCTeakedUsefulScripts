ade6dfbf0dbe393cfd2526d59a67f85626133e05
Berend Veldthuis <57870011+Quantum-Coder826@users.noreply.github.com>
2021-11-04 19:15:08 +0100

    refactor

--- MekInductionMatrix/power.lua
+++ MekInductionMatrix/power.lua
@@ -34,29 +34,22 @@ while true do
     Percantage = math.floor(modem.callRemote(cell,"getEnergyFilledPercentage") * 100)
 
     -- Format all data together
-    dataout = (CurrentPower.."/"..MaxPower.." ".."Percentage".."%\n".."In:"..Input.."/t\n".."Out:"..Output.."/t")
+    dataforremote = (CurrentPower.."/"..MaxPower.." "..Percentage.."%\n".."In:"..Input.."/t\n".."Out:"..Output.."/t")
 
     -- setup monitor
     modem.callRemote(monitor,"clear");modem.callRemote(monitor,"setCursorPos",1,1)
     modem.callRemote(monitor,"write","InductionMatrix:")
-    modem.callRemote(monitor,"setCursorPos",1,2)
 
     -- write all data to monitor
-    modem.callRemote(monitor,"print",dataout)
+    modem.callRemote(monitor,"setCursorPos",1,2)
+    modem.callRemote(monitor,"write",(CurrentPower.."/"..MaxPower))
 
-    -- -- write curret power and maximum power
-    -- modem.callRemote(monitor,"setCursorPos",1,2)
-    -- modem.callRemote(monitor,"write",(CurrentPower.."/"..MaxPower))
+    modem.callRemote(monitor,"setCursorPos",modem.callRemote(monitor,"getCursorPos")+1,2)
+    modem.callRemote(monitor,"write",(Percantage.."%"))
 
-    -- -- write percantage filled
-    -- modem.callRemote(monitor,"setCursorPos",modem.callRemote(monitor,"getCursorPos")+1,2)
-    -- modem.callRemote(monitor,"write",(Percantage.."%"))
-
-    -- -- write current power input and output
-    -- modem.callRemote(monitor,"setCursorPos",1,3)
-    -- modem.callRemote(monitor,"write",("In:"..Input.."/t"))
-    -- modem.callRemote(monitor,"setCursorPos",1,4)
-    -- modem.callRemote(monitor,"write",("Out:"..Output.."/t"))
-
+    modem.callRemote(monitor,"setCursorPos",1,3)
+    modem.callRemote(monitor,"write",("In:"..Input.."/t"))
+    modem.callRemote(monitor,"setCursorPos",1,4)
+    modem.callRemote(monitor,"write",("Out:"..Output.."/t"))
     sleep(0,8) -- Wait a bit to not overload periferals
 end
