9bd699243283a08de0f2b3759a740e5173f280a2
Berend Veldthuis <57870011+Quantum-Coder826@users.noreply.github.com>
2021-11-05 17:29:05 +0100

    code

--- MekInductionMatrix/receve.lua
+++ MekInductionMatrix/receve.lua
@@ -8,9 +8,10 @@ while true do
     term.clear();term.setCursorPos(1,1)
     term.write("InductionMatrix:")
     term.setCursorPos(1,2)
-    
-    local id, message = rednet.receive(protocol)
-    if id == host then
-        term.write(message)
-    end
+
+    local id, message
+    repeat
+        id, message = rednet.receive()
+    until id == host
+    term.write(message)
 end
