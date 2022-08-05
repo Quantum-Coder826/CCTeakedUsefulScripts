57fa779d0fc8e03d2dc617ae7373bbd02b2ee2ea
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-05 21:33:54 +0200

    bug fixes plust data correction

--- MekInductionMatrix/power.lua
+++ MekInductionMatrix/power.lua
@@ -1,7 +1,7 @@
 
 -- vars for periferals and data
 local data = {}
-local battery = "inductionMatrix_0"
+local battery = "inductionPort_0"
 local monitor = "monitor_0"
 local modem = peripheral.find("modem")
 
@@ -10,6 +10,7 @@ local protocol = "battery" -- sets the rednet protocol
     -- TODO Add rednet stuff
 
 local function  correctquantity(n) -- correct int to str and apends proper quantity
+    n = math.floor(n / 2.5) -- divide the value by 2.5 to convert from joules to FE
     if n >= 10^15 then
         return string.format("%.2f PFE", n / 10^15)
     elseif n >= 10^12 then
@@ -29,12 +30,12 @@ while true do -- the loop
     -- collect all the data
     data["CurrentPower"] =  correctquantity(modem.callRemote(battery, "getEnergy"))
     data["MaxPower"] = correctquantity(modem.callRemote(battery, "getMaxEnergy"))
-    data["Input"] = correctquantity(modem.callRemoteq(battery, "getLastInput"))
+    data["Input"] = correctquantity(modem.callRemote(battery, "getLastInput"))
     data["Output"] = correctquantity(modem.callRemote(battery, "getLastOutput"))
-    data["Percent"] = correctquantity(math.floor(modem.callRemote(battery, "getPercentageFilled") * 100))
+    data["Percent"] = correctquantity(math.floor(modem.callRemote(battery, "getEnergyfilledPercentage") * 100))
     
     -- todo: make rednet broadcast the data
 
     -- todo: print to local monitor
-    print(data)
+    print(textutils.serialise(data))
 end
