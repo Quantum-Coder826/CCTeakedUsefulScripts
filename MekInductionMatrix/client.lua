b14aeed9a1ef872cb9f9ca732860be67e0bc444e
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-06 17:16:14 +0200

    portable dectection

--- MekInductionMatrix/client.lua
+++ MekInductionMatrix/client.lua
@@ -1,10 +1,17 @@
 -- local vars
-local wantedProtocol = "battery" -- sets the protocol
+local wantedProtocol = "battery" -- sets the protocol to look for
+local monitor = peripheral.find("monitor_0") -- the name for the connected monitor
+
 
 -- local fuctions
-local function newline()
+local function termNewline()
     local x, y = term.getCursorPos()
     term.setCursorPos(1, y + 1)
+end
+
+local function monitorNewline()
+    local x, y = monitor.getCursorPos()
+    monitor.setCursorPos(1, y + 1)
 end
 
 -- the code
@@ -14,16 +21,27 @@ while true do
     local event, sender, message, protocol = os.pullEvent("rednet_message")
     if protocol == wantedProtocol then
         local data = textutils.unserialise(message)
-        
-        -- data to the terminal
-        term.clear();term.setCursorPos(1,1) -- reset the terminal
-        term.write("Inducionmatrix:")
-        newline()
-        term.write(data["CurrentPower"] .. "/" .. data["MaxPower"] .. " " .. data["Percent"] .. "%")
-        newline()
-        term.write("In:" .. data["Input"] .. "/t")
-        newline()
-        term.write("Out:" .. data["Output"] .. "/t")
     
+        if pocket then
+            -- print data to the terminal
+            term.clear();term.setCursorPos(1,1) -- reset the terminal
+            term.write("Inducionmatrix:")
+            termNewline()
+            term.write(data["CurrentPower"] .. "/" .. data["MaxPower"] .. " " .. data["Percent"] .. "%")
+            termNewline()
+            term.write("In:" .. data["Input"] .. "/t")
+            termNewline()
+            term.write("Out:" .. data["Output"] .. "/t")
+        else
+            -- print data to connecter monitor
+            monitor.clear();monitor.setCursorPos(1,1)
+            monitor.write("Inductionmatrix:")
+            monitorNewline()
+            monitor.write(data["CurrentPower"] .. "/" .. data["MaxPower"] .. " " .. data["Percent"] .. "%")
+            monitorNewline()
+            monitor.write("In:" .. data["Input"] .. "/t")
+            monitorNewline()
+            monitor.write("Out:" .. data["Output"] .. "/t")
+        end
     end
   end
