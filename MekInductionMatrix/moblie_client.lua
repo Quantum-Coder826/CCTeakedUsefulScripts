787f5b7c1e308714cad778b82f3286e40e05032c
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-06 11:58:40 +0200

    writing all the data

--- MekInductionMatrix/moblie_client.lua
+++ MekInductionMatrix/moblie_client.lua
@@ -13,8 +13,17 @@ rednet.open("back")
 while true do
     local event, sender, message, protocol = os.pullEvent("rednet_message")
     if protocol == wantedProtocol then
-        -- todo: write to the monitor
-        print(textutils.unserialise(message))
+        local data = textutils.unserialise(message)
+        
+        -- todo: write to the terminal
+        term.clear();term.setCursorPos(1,1) -- reset the terminal
+        term.write("Inducionmatrix:")
+        newline()
+        term.write(data["CurrentPower"] .. "/" .. data["MaxPower"] .. " " .. data["Percentage"] .. "%")
+        newline()
+        term.write("In:" .. data["Input"] .. "/t")
+        newline()
+        term.write("Out:" .. data["Output"] .. "/t")
     
     end
   end
