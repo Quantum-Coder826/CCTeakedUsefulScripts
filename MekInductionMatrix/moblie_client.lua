a4274514b1e1f7cc63b4c3ca73ddb826e0e61d3b
Quantum-Coder826 <b.veldthuis@gmail.com>
2022-08-06 11:39:09 +0200

    rednet

--- MekInductionMatrix/moblie_client.lua
+++ MekInductionMatrix/moblie_client.lua
@@ -8,13 +8,15 @@ local function newline()
 end
 
 -- the code
+rednet.open("back")
+
 while true do
     local event, sender, message, protocol = os.pullEvent("redent_message") -- set the rednet_message event
     if protocol == wantedProtocol then
 
         local data = textutils.unserialise(message)
         term.clear();term.setCursorPos(1,1) -- reset screen
-        
+
         term.write("InducionMatrix:") -- write all te data
         newline()
         term.write(data["CurrentPower"] .. "/" .. data["MaxPower"] .. " " .. data["Percent"] .. "%")
